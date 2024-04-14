d14dc659646b7713f71d2cc966a80c6be870f342
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-04-13 21:03:30 -0400

    feat: Add validation and refine schema

---
+++ scripts/validate.ps1
@@ -0,0 +1,21 @@
+#requires -modules Prelude
+
+$Referenced = @(
+    'ApplicationCategory'
+    'Artifact'
+    'DevelopmentStatus'
+    'License'
+    'OperatingSystem'
+    'RuntimePlatform'
+    'TargetProduct'
+)
+$Parameters = @{
+    'all-errors' = $true
+    'spec' = 'draft2020'
+    'c' = 'ajv-formats'
+    'd' = '"artifacts/metadata.json"'
+    'r' = "`"./schemas/`{$($Referenced -join ',')`}.json`""
+    's' = '"./schemas/metadata.json"'
+}
+$Command = "./node_modules/.bin/ajv validate $($Parameters | ConvertTo-ParameterString)"
+Invoke-Expression $Command
---
+++ scripts/validate.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -e
+
+./node_modules/.bin/ajv validate \
+    --all-errors \
+    --spec draft2020 \
+    -c ajv-formats \
+    -s ./schemas/Artifact.json \
+    -r "./schemas/{ApplicationCategory,DevelopmentStatus,License,OperatingSystem,RuntimePlatform,TargetProduct}.json" \
+    -d "artifacts/metadata.json" || true
